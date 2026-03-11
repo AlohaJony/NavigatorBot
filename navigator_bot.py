33968fc8772dd1875047f070019ff1ad32a37ddc
Aleksandr Lutsko <aleksandr@Air-Aleksandr.IGD_Rostelecom>
2026-03-11 22:08:19 +0800

    Fix

--- navigator_bot.py
+++ navigator_bot.py
@@ -64,20 +64,20 @@ def main_menu_keyboard():
                     {"type": "link", "text": "📄 Конвертер (⚡)", "url": PDF_BOT_LINK}
                 ],
                 # Второй ряд
-                [
-                    {"type": "link", "text": "🎵 Аудио из видео (в разработке)", "url": AUDIO_BOT_LINK},
-                    {"type": "link", "text": "🗣 Озвучка (в разработке)", "url": TTS_BOT_LINK}
-                ],
+                #[
+                #    {"type": "link", "text": "🎵 Аудио из видео (в разработке)", "url": AUDIO_BOT_LINK},
+                #    {"type": "link", "text": "🗣 Озвучка (в разработке)", "url": TTS_BOT_LINK}
+                #],
                 # Третий ряд
-                [
-                    {"type": "link", "text": "🎨 Генерация (в разработке)", "url": IMAGE_BOT_LINK},
-                    {"type": "callback", "text": "💎 Подписки", "payload": "subscriptions"}
-                ],
+                #[
+                #    {"type": "link", "text": "🎨 Генерация (в разработке)", "url": IMAGE_BOT_LINK},
+                #    {"type": "callback", "text": "💎 Подписки", "payload": "subscriptions"}
+                #],
                 # Нижняя строка на всю ширину (две кнопки рядом)
-                [
-                    {"type": "callback", "text": "💰 Мой баланс", "payload": "balance"},
-                    {"type": "callback", "text": "💳 Пополнить", "payload": "topup_menu"}
-                ]
+                #[
+                #    {"type": "callback", "text": "💰 Мой баланс", "payload": "balance"},
+                #    {"type": "callback", "text": "💳 Пополнить", "payload": "topup_menu"}
+                #]
             ]
         }
     }
